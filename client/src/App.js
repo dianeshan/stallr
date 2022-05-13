b4f4ddb718682f3e15060fb738f2848a90bd42ec
Diane Shan <dianeshan40785@gmail.com>
2022-05-12 22:00:42 -0700

    Add some routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+
 import Home from './pages/Home';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        {/* <Route path="/explore" element={<Explore />} />
+        <Route path="/profile" element={<Profile />} /> */}
+      </Routes>
     </div>
   );
 }
