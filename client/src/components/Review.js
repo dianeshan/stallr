224cab553754fe6b4256e59389833ae7c9777944
Wes <wmont012@ucr.edu>
2022-05-19 04:53:23 -0700

    Reviews are a little cleaner

--- client/src/components/Review.js
+++ client/src/components/Review.js
@@ -1,13 +1,18 @@
 import { Card, Button } from 'react-bootstrap';
 import toiletpic from '../resources/images/temp-toilet.jpeg';
+import "./Review.css";
 
 const Review = ({ account, date, toiletlocation, review, photos}) => {
     return (
         <Card className="w-50">
+            <Card.Header>
+                <Card.Text>Location</Card.Text>
+                <Card.Text>Stars</Card.Text>
+            </Card.Header>
             <Card.Img variant="top" src={toiletpic} />
             <Card.Body>
-                <Card.Text>Account Date Location</Card.Text>
-                <Card.Text>Stars</Card.Text>
+                <Card.Text>Account </Card.Text>
+                <Card.Text>Date</Card.Text>
                 <Card.Text>
                     Review Text
                 </Card.Text>
