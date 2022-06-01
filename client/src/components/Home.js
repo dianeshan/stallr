54881bea57ce060291477c1744c176402c529955
Diane Shan <dianeshan40785@gmail.com>
2022-06-01 15:06:34 -0700

    Attempts to add vanta background

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,5 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Container, Modal, Button } from "react-bootstrap";
+import WAVES from "vanta/dist/vanta.waves.min";
+import * as THREE from "three";
 
 import ReviewService from "../services/review.service";
 import AuthService from "../services/auth.service";
@@ -11,6 +13,8 @@ const Home = () => {
   const [reviews, setReviews] = useState([]);
   const [currentIndex, setCurrentIndex] = useState(-1);
   const [currentUser, setCurrentUser] = useState(undefined);
+  const [vantaEffect, setVantaEffect] = useState(0);
+  const myRef = useRef(null);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
@@ -22,6 +26,28 @@ const Home = () => {
       setCurrentUser(user);
     }
   }, []);
+
+  useEffect(() => {
+    if (!vantaEffect) {
+      setVantaEffect(
+        WAVES({
+          el: myRef.current,
+          THREE: THREE,
+          mouseControls: true,
+          touchControls: true,
+          gyroControls: false,
+          minHeight: 200.0,
+          minWidth: 200.0,
+          scale: 1.0,
+          scaleMobile: 1.0,
+          color: 0x838f9b,
+        })
+      );
+    }
+    return () => {
+      if (vantaEffect) vantaEffect.destroy();
+    };
+  }, [vantaEffect]);
 
   const setActiveReview = (user, index) => {
     setCurrentIndex(index);
@@ -39,42 +65,44 @@ const Home = () => {
   };
 
   return (
-    <Container className="pt-4">
-      {currentUser && (
-        <div className="centered">
-          <Button variant="info" onClick={handleShow}>
-            New Review
-          </Button>
-          <Modal show={show} onHide={handleClose}>
-            <Modal.Header closeButton>
-              <Modal.Title>New Review</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-              <NewReview />
-            </Modal.Body>
-          </Modal>
-        </div>
-      )}
-      {reviews &&
-        reviews.map((review, index) => (
-          <div
-            className={index === currentIndex ? "active" : ""}
-            onClick={() => setActiveReview(review, index)}
-            key={index}
-          >
-            <Review
-              id={review._id}
-              username={review.username}
-              date={review.date}
-              description={review.description}
-              rating={review.rating}
-              location={review.location}
-              comments={review.comments}
-              images={review.images}
-            />
+    <div ref={myRef}>
+      <Container className="pt-4">
+        {currentUser && (
+          <div className="centered">
+            <Button variant="info" onClick={handleShow}>
+              New Review
+            </Button>
+            <Modal show={show} onHide={handleClose}>
+              <Modal.Header closeButton>
+                <Modal.Title>New Review</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                <NewReview />
+              </Modal.Body>
+            </Modal>
           </div>
-        ))}
-    </Container>
+        )}
+        {reviews &&
+          reviews.map((review, index) => (
+            <div
+              className={index === currentIndex ? "active" : ""}
+              onClick={() => setActiveReview(review, index)}
+              key={index}
+            >
+              <Review
+                id={review._id}
+                username={review.username}
+                date={review.date}
+                description={review.description}
+                rating={review.rating}
+                location={review.location}
+                comments={review.comments}
+                images={review.images}
+              />
+            </div>
+          ))}
+      </Container>
+    </div>
   );
 };
 
